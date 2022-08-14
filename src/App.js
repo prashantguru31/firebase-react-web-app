a668d3134bae865007fa914b4477da2ff75f03a4
prashantguru31 <prashantguru31@gmail.com>
2022-08-14 22:41:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+import firebase from './FirebaseConfig';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <div className="title-row">
+        <h1 className="title">Firebase Web App</h1>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
